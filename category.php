56553d4594422b2808fc4b5ca79f4afc59602936
danielsamyy <danielsamy205@gmail.com>
2024-10-31 14:24:02 +0300

    Add HTML and CSS for category page with product links and navigation bar

---
+++ category.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>category</title>
+        <link rel="stylesheet"  href="https://fonts.googleapis.com/css?family=Great+Vibes">
+        <link rel="icon" type="image/x-icon" href="./images/blury.jpg">
+        <style>
+            h1{ 
+                font-family:"Great Vibes", cursive;
+                color: black;
+                font-weight: bolder;
+                font-size: xx-large;
+                margin-left: 207px;
+                margin-bottom: 80px;
+            }
+            body{
+                background-size: cover;
+                background-color:bisque;
+    
+            }
+            nav ul{
+    list-style-type: none;
+    text-align: center;
+    margin: 0;
+}
+    nav ul li{
+        display: inline;
+    }
+nav ul li a {
+    display: inline-block;
+    width: 140px;
+    text-decoration:none ;
+    padding: 5px;
+    color: white;
+    font-family: 'Times New Roman', Times, serif;
+    font-weight: bold;
+    font-size: large;
+}
+nav{
+    background-color: #BF0505;
+}
+nav ul li a:hover{
+    background-color:rgb(5, 116, 5);
+    font-weight: bolder;
+    font-size: large;
+    font-family: Arial, Helvetica, sans-serif;
+}
+           
+
+            table{
+                margin: auto;
+            }
+            table tr td{
+                padding-left: 50px;
+            }
+            table tr td p{
+                color:black;
+                font-weight: bold;
+                font-size: larger;
+                font-family: 'Times New Roman', Times, serif;
+            }
+          
+           
+            
+        </style>
+    </head>
+    <body>
+        <nav>
+            <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="feedback.php">Feedback</a></li>
+            <li><a href="about.php">About Us</a></li>
+            </ul>
+        </nav>
+        <header>
+                <h1>Christmas Gorgeous categories <br> Here are our three categories!!</h1></cente>
+        </header>
+      
+        <table>
+            <tr>
+                <td><a href="pj_products.php"><img src="./images/pg4.jpg" alt="can't download this image" width="400" height="400"></a>
+                <p><i>Make the season merry and bright  with our collection of themed pijamas<br> click image to see all the products</i></p></td>
+                <td><a href="mugs_product.php"><img src="./images/mug10.webp" alt="can't download this image" width="400" height="400"></a>
+                <p><i>Get into the festive spirit with our merry and bright christmas mug collections <br>click image to see all the products</i></p></td>
+                <td><a href="decoration_product.php"><img src="./images/dec2.jpg" alt="can't download this image" width="400" height="400"></a>
+                <p><i>Step into a winterwonderland with our magical christmas decorations<br> click image to see all the  products</i></p></td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
