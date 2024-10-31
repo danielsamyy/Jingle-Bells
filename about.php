8a956e7f7be072d69a66f581f704c12a6ce218f9
danielsamyy <danielsamy205@gmail.com>
2024-10-31 14:03:54 +0300

    Style 'About Us' page with festive colors, improved layout, and button hover effects for enhanced user experience

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="icon" type="image/x-icon" href="./images/blury.jpg">
+    <link rel="stylesheet" href="about.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet"  href="https://fonts.googleapis.com/css?family=Great+Vibes">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="category.php">Category</a></li>
+            <li><a href="feedback.php">Feedback</a></li>
+            <li><a href="about.php">About Us</a></li>
+        </ul>
+    </nav>
+    <h2> About Us <i class='bx bx-broadcast'></i></h12>
+        <h1>Welcome to <i class='bx bxs-tree' style='color:#189f12'  ></i>Jingle Bells.com</h1>
+    <table>
+        <tr>
+            <td id="first"><img src="./images/aboutus.webp" alt="" height="235px" width="247px"></td>
+            <td id="second"><img src="./images/pj.webp" alt="" height="180px" width="200px"></td>
+        </tr>
+        <tr>
+            <td id="third"><img src="./images/mug10.webp" alt="" height="175px" width="195px"></td>
+            <td id="fourth"><img src="./images/dec2.jpg" alt="" height="250px" width="237px"></td>
+        </tr>
+    </table>
+    <p><i>
+     At JingleBells.com, we are passionate about celebrating the magic and wonder of Christmas.
+      Our mission <br> is to create a joyful and festive online destination where people can immerse themselves in the spirit of the holiday season. <br> <br>
+      Our team of dedicated Christmas enthusiasts is committed to curating the best content, ideas, and inspiration to make your Christmas season truly magical. <br> <br>
+      We believe in the power of Christmas to bring people together, create cherished memories, and kindle the warmth of love and joy in every heart. <br> <br>
+      We take pride in our vibrant and supportive community of fellow Christmas lovers. Our interactive forums <br> and social media groups provide  a platform for like-minded individuals
+       to connect, share experiences, and spread the joy of the season. Together, we create a virtual family bonded by the love for Christmas. <br> <br>
+        Thank you for joining us in celebrating the most wonderful time of the year. We wish you a Christmas filled with laughter, love, and endless joy.
+         Together, let's make this Christmas season truly unforgettable!</i>
+    </p>
+    <div>
+        <a href="index.php"><button><i>Read more..</i></button></a>
+    </div>
+    
+
+</body>
+</html>
